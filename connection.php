5a68aad01f10500e13fb80979bdd67d402edd66f
PranabNandy <34576104+PranabNandy@users.noreply.github.com>
2020-12-25 23:03:34 +0545

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>Connection</title>
+</head>
+<body>
+<?php
+	//include_once("connection.php");
+	//echo "<h1>0.Joi Baba Sai Nath</h1>";
+	$user='root';
+	$pass='';
+	$db='nandy';
+	$con=new mysqli('localhost',$user,$pass,$db);
+
+
+
+
+
+
+
+
+?>
+
+
+
+	
+</body>
+
+</html>
+
